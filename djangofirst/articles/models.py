9ea4aabe65eed6164b73db9399b1673af93f5edd
Ankit <66716237+madboi01@users.noreply.github.com>
2021-01-13 23:11:40 +0530

    interacted with database with Django ORM

--- djangofirst/articles/models.py
+++ djangofirst/articles/models.py
@@ -8,3 +8,5 @@ class Article(models.Model):
     data=models.DateTimeField(auto_now_add=True)
     #add in thumbnail later
     #add in author later
+    def __str__(self):
+        return self.title
