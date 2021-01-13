534739a6163796d164cf65562032aa21f94c3928
Ankit <66716237+madboi01@users.noreply.github.com>
2021-01-13 23:43:32 +0530

    django admin created

--- djangofirst/djangofirst/urls.py
+++ djangofirst/djangofirst/urls.py
@@ -1,8 +1,10 @@
 
 from django.urls import path, include
+from django.contrib import admin
 from . import views
 
 urlpatterns = [
+    path('admin/',admin.site.urls),
     path('articles/',include('articles.urls')),
     path('about/',views.about),
     path('',views.homepage),
