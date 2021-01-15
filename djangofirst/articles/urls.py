9f81afd789e38bba58c322a5b32f1103cb6dfca5
Ankit <66716237+madboi01@users.noreply.github.com>
2021-01-15 18:16:23 +0530

    Named URLs

--- djangofirst/articles/urls.py
+++ djangofirst/articles/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .import views
 
+app_name="article"
+
 urlpatterns = [
-    path('',views.articles_list),
-    path('<slug:slug>/',views.article_detail),
+    path('',views.articles_list, name="list"),
+    path('<slug:slug>/',views.article_detail, name="detail"),
 ]
