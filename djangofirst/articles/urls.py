4187a9ddce2f840f8784b99455dc35c79eec065e
Ankit <66716237+madboi01@users.noreply.github.com>
2021-01-17 20:34:11 +0530

    Requiring login for creating articles

--- djangofirst/articles/urls.py
+++ djangofirst/articles/urls.py
@@ -5,5 +5,6 @@ app_name="article"
 
 urlpatterns = [
     path('',views.articles_list, name="list"),
+    path('create/',views.article_create,name="create"),
     path('<slug:slug>/',views.article_detail, name="detail"),
 ]
